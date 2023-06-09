294e359f5360f49f7b7234c3ad44d3ca75cd4db5
Franco Tevez <tevezzfranco@gmail.com>
2023-06-08 21:41:17 -0300

    v1 reflex

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react'
+import './index.css'
+
+function App() {
+  const [play, setPlay] = useState("jugar")
+  const [contador, setContador] = useState(0)
+  const [score, setScore] = useState(1)
+  const [position, setPosition] = useState([])
+  const [animation, setAnimation] = useState("aparece")
+  const [colores, setColores] = useState("crimson")
+  const [frase, setFrase] = useState("")
+
+  function funcFrase(){
+    if(Math.round((contador / 10) * 100) / 100 < 3){
+      setFrase("Xq tocaste terminar si no terminaste?") 
+    }else if(Math.round((contador / 10) * 100) / 100 >= 3 && Math.round((contador / 10) * 100) / 100 < 4){
+      setFrase("Sos una bestia, mira el tiempo que metiste!")
+    }else if(Math.round((contador / 10) * 100) / 100 >= 4 && Math.round((contador / 10) * 100) / 100 < 5.5){
+      setFrase("Estás muy rápido, no estás jugando desde el celu no?")
+    }else if(Math.round((contador / 10) * 100) / 100 >= 5.5 && Math.round((contador / 10) * 100) / 100 < 8){
+      setFrase("Metiste un buen tiempo, pero podés mejorarlo papá")
+    }else{
+      setFrase("Sos un desastre... Intentá de nuevo te lo pido por favor.")
+    }
+  }
+
+  useEffect(() => {
+    let interval;
+    if(play == "reiniciar"){
+      if(score < 10){
+        setFrase("Terminaste antes, este tiempo no cuenta.")
+      }
+    }
+    if(play == "terminar"){ 
+      setScore(1)
+      let top =  Math.floor(Math.random() * 100)
+      let left = Math.floor(Math.random() * 100)
+      if(top > 80){
+        top -= 10
+      }
+      if(left > 80){
+        left -= 10
+      }
+      setPosition([
+        top, left
+      ])
+      setContador(0)
+    }
+    if(play == "terminar"){
+      interval = setInterval(() => { setContador((prevContador) => prevContador +1)  }, 100);
+    }
+
+    return () => clearInterval(interval)
+  }, [play])
+  
+  function clickFigura() {
+    animation == "aparece" ? setAnimation("aparece2") : setAnimation("aparece")
+    setScore((prevScore) => prevScore + 1)
+    let top =  Math.floor(Math.random() * 100)
+    let left = Math.floor(Math.random() * 100)
+    if(top > 80){
+      top -= 10
+    }
+    if(left > 80){
+      left -= 10
+    }
+    setPosition([
+      top, left
+    ])
+    if(score == 10){
+      funcFrase()
+      setPlay("reiniciar")
+    }
+  }
+  
+  return (
+    <div className='contenedor'>
+      <span>{Math.round((contador / 10) * 100) / 100} segundos</span>
+      <div className='contColores'>
+        <button type='button' className='botonColores crimson' onClick={() => setColores("crimson")}></button>
+        <button type='button' className='botonColores white' onClick={() => setColores("white")}></button>
+        <button type='button' className='botonColores yellow' onClick={() => setColores("yellow")}></button>
+        <button type='button' className='botonColores sienna' onClick={() => setColores("sienna")}></button>
+        <button type='button' className='botonColores skyblue' onClick={() => setColores("skyblue")}></button>
+        <button type='button' className='botonColores violet' onClick={() => setColores("blueviolet")}></button>
+      </div>
+      <div className='contenedorFigura'>
+        {play == "reiniciar" && <h2>{frase}</h2>}
+        {play == "jugar" && <h2>Hacé 10 clicks en el menor tiempo posible papito dale.</h2>}
+        {play == "terminar" && <figure onClick={clickFigura} style={{top: `${position[0]}%`, left: `${position[1]}%`, height: `${48 - score * 2}px`, width: `${48 - score * 2}px`, animation: `${animation} 0.2s`, backgroundColor: `${colores}`}}></figure>}
+      </div>
+      <div className='contToques'>
+        <p>TOQUES: {score - 1}</p>
+      </div>
+      <div className='contBoton'>
+        {play == "jugar" && <button onClick={() => setPlay("terminar")} type='button' className='botonJuego'>Jugar</button>}
+        {play == "terminar" && <button onClick={() => setPlay("reiniciar")} type='button' className='botonJuego'>Terminar</button>}
+        {play == "reiniciar" && <button onClick={() => setPlay("terminar")} type='button' className='botonJuego'>Reiniciar</button>}
+      </div>
+    </div>
+  )
+}
+
+export default App
